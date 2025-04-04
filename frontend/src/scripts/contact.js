947c9dfe7ca03c901c44df69c0ab78249e07e7d2
Suvrat Chauhan <chauhansuvrat344@gmail.com>
2025-04-04 23:42:02 +0530

    Add contact script

---
+++ frontend/src/scripts/contact.js
@@ -0,0 +1,33 @@
+export default{
+  data: function(){
+    return{
+      contactInfo : [
+        {
+          "title" : "Address:",
+          "value" : "4578 Marmora Road, Glasgow",
+          "icon" : "fa fa-map-marker"
+        },
+        {
+          "title" : "Phone:",
+          "value" : "(719) 445-2808;(719) 445-2809",
+          "icon" : "fa fa-phone"
+        },
+        {
+          "title" : "Fax:",
+          "value" : "(719) 445-2808",
+          "icon" : "fa fa-fax"
+        },
+        {
+          "title" : "E-mail:",
+          "value" : "info@demolink.org",
+          "icon" : "fa fa-envelope"
+        },
+        {
+          "title" : "Timing:",
+          "value" : "Mon - Fri: 08.00 - 17.00 Sat: 09.00 - 12.00",
+          "icon" : "fa fa-clock"
+        }
+      ]
+    }
+  }
+}
