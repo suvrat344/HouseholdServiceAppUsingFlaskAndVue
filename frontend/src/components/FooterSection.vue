5ead8a4a136fef84fefaf888b6ebec91c8204a15
Suvrat Chauhan <chauhansuvrat344@gmail.com>
2025-04-04 23:40:35 +0530

    Add footer component

---
+++ frontend/src/components/FooterSection.vue
@@ -0,0 +1,13 @@
+<template>
+  <footer id="footer-section">
+    <div class="footer">
+      <img src="../assets/images/footer.jpg" alt="Logo">
+    </div>
+  </footer>
+</template>
+
+<script>
+  export default{
+    name: 'FooterSection'
+  }
+</script>
